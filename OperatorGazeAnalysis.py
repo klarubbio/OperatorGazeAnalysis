2c7ca947b8a94e84efad128084f3b0c635f0c9dc
klarubbio <klarubbio@ufl.edu>
2023-07-17 16:28:05 -0400

    plots

--- OperatorGazeAnalysis.py
+++ OperatorGazeAnalysis.py
@@ -1,5 +1,8 @@
 import saccademodel 
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 # frame rate in fps
 frame_rate = float(input("Enter frame rate of eye tracker (HoloLens default is 30fps): "))
@@ -11,26 +14,48 @@ frame_interval = frame_rate * time_interval
 
 input_eye_points = []
 
+saccade_durations = []
+
+frame_stamps = []
+
+errors = []
+
+
 with open('machinesample_BIG.csv', newline = '\n') as csvfile:
     data = csv.DictReader(csvfile, delimiter = ',')
     frame_count = 0;
+    
     for row in data:
         non_nans = 0
-        if row['enabled'] != 'enabled' and eval(row['enabled']) and eval(row['valid']):
+        if row['enabled'] != 'enabled' and row['enabled'] == 'TRUE' and row['valid'] == 'TRUE':
             input_eye_points.append([float(row['gaze_origin_x']) + float(row['gaze_direction_x']), float(row['gaze_origin_y']) + float(row['gaze_direction_y'])])
             non_nans = 1
         else:
             input_eye_points.append([None, None])
 
-        if frame_count % frame_interval == 0 and frame_count != 0 and non_nans:
-            results = saccademodel.fit(input_eye_points)
+        if frame_count % frame_interval == 0 and frame_count != 0:
+            
 
-            print("info at frame: ", frame_count)
-            if "saccade_points" in results.keys():
-                print('saccade duration: ', len(results["saccade_points"]) / frame_rate)
-            print('error: ', results["mean_squared_error"])
+            if non_nans:
+                results = saccademodel.fit(input_eye_points)
+                print("info at frame: ", frame_count)
+                if "saccade_points" in results.keys():
+                    print('saccade duration: ', len(results["saccade_points"]) / frame_rate)
+                    saccade_durations = np.append(saccade_durations, len(results["saccade_points"]) / frame_rate)
+                    frame_stamps = np.append(frame_stamps, frame_count);
+                print('error: ', results["mean_squared_error"])
+                errors = np.append(errors, results["mean_squared_error"]);
+                
             input_eye_points.clear()
 
+
         frame_count += 1
+        if frame_count > 200000:
+            break
+
+plt.plot(frame_stamps, saccade_durations)
+plt.xlabel("Frame")
+plt.ylabel("Saccade Duration (frames per interval)")
+plt.show()
   
 
