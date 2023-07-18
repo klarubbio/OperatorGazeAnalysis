b7a28d7b5a5ea94a99bc403b5142bfa6e9da1457
klarubbio <klarubbio@ufl.edu>
2023-07-18 15:43:10 -0400

    aoi graphs

--- OperatorGazeAnalysis.py
+++ OperatorGazeAnalysis.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+from pygaze import *
+
 # frame rate in fps
 frame_rate = float(input("Enter frame rate of eye tracker (HoloLens default is 30fps): "))
 
@@ -16,26 +18,40 @@ input_eye_points = []
 
 saccade_durations = []
 
-frame_stamps = []
+frame_stamps = [0]
 
 errors = []
 
+target_hits = {}
 
-with open('machinesample_BIG.csv', newline = '\n') as csvfile:
+
+with open('machinesample20230718112820.csv', newline = '\n') as csvfile:
     data = csv.DictReader(csvfile, delimiter = ',')
     frame_count = 0;
     
     for row in data:
+        if frame_count % frame_interval == 0 and frame_count != 0:
+            frame_stamps = np.append(frame_stamps, frame_count / frame_rate);
+        frame_count += 1
+
+        if row['hit_name'] != "" and row['hit_name'] != None:
+            if target_hits.get(row["hit_name"]) == None:
+                target_hits[row["hit_name"]] = []
+            # add 0s for intervals with no hits
+            for i in range(0, len(frame_stamps) - len(target_hits[row["hit_name"]])):
+                    target_hits[row["hit_name"]].append(0)
+            target_hits[row["hit_name"]][len(frame_stamps)-1] += 1
+
+        
+
+        '''
         non_nans = 0
         if row['enabled'] != 'enabled' and row['enabled'] == 'TRUE' and row['valid'] == 'TRUE':
             input_eye_points.append([float(row['gaze_origin_x']) + float(row['gaze_direction_x']), float(row['gaze_origin_y']) + float(row['gaze_direction_y'])])
             non_nans = 1
         else:
             input_eye_points.append([None, None])
-
         if frame_count % frame_interval == 0 and frame_count != 0:
-            
-
             if non_nans:
                 results = saccademodel.fit(input_eye_points)
                 print("info at frame: ", frame_count)
@@ -52,10 +68,26 @@ with open('machinesample_BIG.csv', newline = '\n') as csvfile:
         frame_count += 1
         if frame_count > 200000:
             break
+        ''' 
 
-plt.plot(frame_stamps, saccade_durations)
-plt.xlabel("Frame")
-plt.ylabel("Saccade Duration (frames per interval)")
+
+for data in target_hits.items():
+    for i in range(0, len(frame_stamps) - len(data[1])):
+        data[1].append(0)
+    if "Controller" in data[0]:
+        plt.plot(frame_stamps, data[1], label = data[0], color = "blue")
+    elif "Display" in data[0]:
+        plt.plot(frame_stamps, data[1], label = data[0], color = "red")
+    else:
+        plt.plot(frame_stamps, data[1], label = data[0])
+    
+    plt.legend()
+
+
+
+
+plt.xlabel("Seconds")
+plt.ylabel("Fixations in " + str(time_interval) + " Second Interval")
 plt.show()
   
 
